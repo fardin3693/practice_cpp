afd8a77bfb7197c05025093df31bcc98225200f9
fardin3693 <fardinulislam369@gmail.com>
2026-07-11 19:14:07 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  // this is a comment
+  /*
+     this is a multi
+     line comment.
+      */
+  std::cout << "Hello world!" << std::endl;
+  std::cout << "its's realy good" << std::endl;
+  return 0;
+}
---
+++ variable.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+int main() {
+
+  // int x; // declaration
+  // int y;
+  // x = 5; // assignment
+  // y = 6; // assignment
+  // int sum = x + y; // operation
+
+  // std::cout << "x = " << x << std::endl;
+  // std::cout << "y = " << y << '\n';
+  // std::cout << "sum = " << sum << std::endl;
+
+  int age = 21;
+  int year = 2025;
+
+  float days = 7.5; // for storing a minimum amount of floaing point number.
+  double price = 10.99;
+  double gpa = 2.5;
+  double temparature = 25.1;
+
+  char grade = 'A'; // can store only one charecter
+  char initial = 'B';
+  char currency = '$';
+
+  bool student = false; // bool is a boolean data type.
+  bool power = false;
+
+  std::string name = "Fardin"; // seems to be python string data type
+  std::string food = "Nothing";
+  std::string day = "Saturday";
+  std::string school = "Nasima kadir Molla High Shcool and Homes";
+
+  std::cout << day << std::endl;
+  std::cout << school << std::endl;
+  std::cout << food << std::endl;
+  std::cout << name << std::endl;
+  std::cout << power << std::endl;
+  std::cout << student << std::endl;
+  std::cout << currency << std::endl;
+  std::cout << initial << std::endl;
+  std::cout << grade << std::endl;
+  std::cout << age << std::endl;
+  std::cout << year << std::endl;
+  std::cout << days << std::endl;
+  std::cout << price << std::endl;
+  std::cout << gpa << std::endl;
+  std::cout << temparature << std::endl;
+  // pring string along with variable.
+  std::cout << "Hello" << " " << name << std::endl;
+  std::cout << "You are " << age << "years old" << std::endl;
+
+  return 0;
+}
